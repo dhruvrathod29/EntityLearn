001026660c6b27eb71b3522b6700ab728172e9a3
Dhruv Rathod <86467492+dhruvrathod29@users.noreply.github.com>
2022-12-12 22:46:34 +0530

    'Student_Model'

---
+++ EntityLearn/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using MessagePack;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityLearn.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentID { get; set; }
+
+
+        [Required(ErrorMessage = "Enter Name")]
+        [StringLength(250, ErrorMessage = "name must be less than or equals to 250 charcter")]
+        public string StudentName { get; set;}
+
+
+        [Required(ErrorMessage ="Enter Enrollment no")]
+        public int EnrollmentNo { get; set; }
+        public string Address { get; set; }
+
+        public string StudentEmail { get; set;}
+        public string ContactNo { get; set;}
+        public DateTime BirthDate { get; set;}
+
+
+
+    }
+}
