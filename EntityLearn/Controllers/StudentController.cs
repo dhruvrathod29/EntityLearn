795db2b43b985d4eef88fef5a7f66c5bda5851ea
Dhruv Rathod <86467492+dhruvrathod29@users.noreply.github.com>
2022-12-13 22:14:45 +0530

    'Student_List_psge','Student_AddEdit_page','Index_Page'

--- EntityLearn/Controllers/StudentController.cs
+++ EntityLearn/Controllers/StudentController.cs
@@ -1,16 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLearn.Data;
+using EntityLearn.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EntityLearn.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly ProductDbContext _db;
+        
+
+        public StudentController(ProductDbContext db)
+        {
+            _db = db;   
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_db.Students.ToList()) ;
         }
         public IActionResult Add()
         {
             return View();  
+        }
+
+        [HttpPost]
+        public IActionResult Add(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Students.Add(student);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
         }
 
     }
